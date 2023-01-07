bd61e91caa9e90297a9c8689e54ea4b07afdd078
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-01-07 18:42:30 +0100

    New package version for io.getready.rgn.signin.email, version: 0.0.14 internal revision 4

--- Runtime/src/EmailSignInModule.cs
+++ Runtime/src/EmailSignInModule.cs
@@ -17,17 +17,19 @@ namespace RGN.Modules.SignIn
 
         public void OnSignUpWithEmail(string email, string password)
         {
+            rgnCore.Dependencies.Logger.Log("EmailSignInModule]: OnSignUpWithEmail(" + email + ", " + string.IsNullOrEmpty(password) + ")");
             var credential = rgnCore.ReadyMasterAuth.emailAuthProvider.GetCredential(email, password);
 
-            rgnCore.ReadyMasterAuth.CurrentUser.LinkAndRetrieveDataWithCredentialAsync(credential).ContinueWith(task =>
-            {
+            rgnCore.ReadyMasterAuth.CurrentUser.LinkAndRetrieveDataWithCredentialAsync(credential).ContinueWith(task => {
                 if (task.IsCanceled)
                 {
+                    rgnCore.Dependencies.Logger.LogWarning("EmailSignInModule]: LinkAndRetrieveDataWithCredentialAsync was cancelled");
                     return;
                 }
 
                 if (task.IsFaulted)
                 {
+                    Utility.ExceptionHelper.PrintToLog(rgnCore.Dependencies.Logger, task.Exception);
                     FirebaseAccountLinkException firebaseAccountLinkException = task.Exception.InnerException as FirebaseAccountLinkException;
                     if (firebaseAccountLinkException != null && firebaseAccountLinkException.ErrorCode == (int)AuthError.CredentialAlreadyInUse)
                     {
@@ -61,30 +63,30 @@ namespace RGN.Modules.SignIn
 
         public void OnSignInWithEmail(string email, string password)
         {
-            rgnCore.ReadyMasterAuth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
-            {
+            rgnCore.ReadyMasterAuth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
                 if (task.IsCanceled)
                 {
+                    rgnCore.Dependencies.Logger.LogWarning("EmailSignInModule]: SignInWithEmailAndPasswordAsync was cancelled");
                     SignOutFromEmail();
                     return;
                 }
 
                 if (task.IsFaulted)
                 {
+                    Utility.ExceptionHelper.PrintToLog(rgnCore.Dependencies.Logger, task.Exception);
                     SignOutFromEmail();
                     rgnCore.SetAuthCompletion(EnumLoginState.Error, EnumLoginError.Unknown);
                     return;
                 }
 
-                rgnCore.Dependencies.Logger.Log("[GoogleSignInModule]: Email/Password, signed in");
+                rgnCore.Dependencies.Logger.Log("[EmailSignInModule]: Email/Password, signed in");
             },
             TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         public void SendPasswordResetEmail(string email)
         {
-            rgnCore.ReadyMasterAuth.SendPasswordResetEmailAsync(email).ContinueWith(task =>
-            {
+            rgnCore.ReadyMasterAuth.SendPasswordResetEmailAsync(email).ContinueWith(task => {
                 if (task.IsCanceled)
                 {
                     rgnCore.Dependencies.Logger.LogError("[EmailSignInModule]: SendPasswordResetEmailAsync was canceled.");
@@ -93,6 +95,7 @@ namespace RGN.Modules.SignIn
 
                 if (task.IsFaulted)
                 {
+                    Utility.ExceptionHelper.PrintToLog(rgnCore.Dependencies.Logger, task.Exception);
                     rgnCore.Dependencies.Logger.LogError("[EmailSignInModule]: SendPasswordResetEmailAsync encountered an error: " +
                                    task.Exception);
                     return;
@@ -100,7 +103,8 @@ namespace RGN.Modules.SignIn
 
                 SignOutFromEmail();
                 rgnCore.Dependencies.Logger.Log("[EmailSignInModule]: Password reset email sent successfully.");
-            });
+            },
+            TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         public void SignOutFromEmail()
